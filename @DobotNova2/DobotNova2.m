2155eb1c38a332923e365e5de364bc4162b44286
greese <lovchevmaks@mail.ru>
2024-09-29 19:06:33 +1000

    Complete class for DobotNova2, some design options environment elements
    Class for DobotNova2 now fully functional

--- @DobotNova2/DobotNova2.m
+++ @DobotNova2/DobotNova2.m
@@ -8,28 +8,34 @@ classdef DobotNova2 < RobotBaseClass
     methods
         %% Define robot function (Constructor)
         function self = DobotNova2(baseTr)
-            % Call to create the robot model
-            self.CreateModel();
-            
-            % If no base transform is provided, use the identity matrix
-            if nargin < 1
-				baseTr = eye(4);  % Identity matrix as default base transformation
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 % Nothing passed
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
             end
-            
-            % Set the base transformation of the robot
-            self.model.base = self.model.base.T * baseTr * trotx(pi/2) * troty(pi/2);
-
-            % Plot the robot with its defined color scheme
+          
+            self.CreateModel();
+			self.model.base = self.model.base.T * baseTr;
+            self.model.tool = self.toolTr;
             self.PlotAndColourRobot();
+
+            drawnow
         end
         
         %% Create the robot model (Kinematic chain)
         function CreateModel(self)
-            link(1) = Link('d',0.2234,'a', 0,'alpha',pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
-            link(2) = Link('d',0.1175,'a',0.280,'alpha',0,'qlim', deg2rad([0 180]), 'offset',0);
-            link(3) = Link('d',-0.1175,'a',0.225,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
+            link(1) = Link('d',0.2234,'a', 0,'alpha',-pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            link(2) = Link('d',0,'a',-0.280,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            link(3) = Link('d',0,'a',-0.225,'alpha',0,'qlim', deg2rad([-360 360]), 'offset', 0);
             link(4) = Link('d',0.1175,'a',0,'alpha',pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
-            link(5) = Link('d',0.120,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            link(5) = Link('d',0.12,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
             link(6) = Link('d',	0.088,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
             
             % Create the serial link model for the UR3 on a linear rail
