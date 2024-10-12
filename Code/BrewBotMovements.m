6d859a0a0cc8ae05d1019fbde86aa082588dddad
LazarevMike <mike.lazarev4321@gmail.com>
2024-10-12 13:25:49 +1100

    Added drink functions to the class
    Added drink methods to the class, which utilizes helper functions to create these drinks

--- Code/BrewBotMovements.m
+++ Code/BrewBotMovements.m
@@ -1,39 +1,112 @@
 classdef BrewBotMovements  
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
-    
+    % Class responsible for controlling the movements of the UR3e and DobotNova2 
+    % This class includes methods for creating various types of drinks
+    % (espresso, latte, iced coffee, tea) and helper functions for 
+    % specific steps in the brewing process.
+
     properties
-        Property1
+        Property1  % Placeholder property, can be customized as per BrewBot's requirements.
     end
     
     methods
-        function obj = untitled(inputArg1,inputArg2)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
+        %% Functions to create drinks (espresso, latte, icedCoffee, tea)
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
-        % latte = pick cup + createEspresso + warm up milk + close lid + give it to customer 
-        
-        %% MAIN DRINK FUCNTIONS
-        fucntion Latte % creates latte 
-            brewEspressoShot
-            frothMilk
-            giveDrinkToCustomer 
+        function espressoCreate()
+            % Function that creates an espresso drink.
+            % Includes steps:
+            % 1. Brew espresso (espressoBrew)
+            % 2. Finish the drink (finishDrink)
+
         end
 
+        function latteCreate()
+            % Function that creates a latte drink.
+            % Includes steps:
+            % 1. Brew espresso (espressoBrew)
+            % 2. Add milk to the cup (addMilk)
+            % 3. Steam the liquid (steamLiquid)
+            % 4. Pour milk into the cup (pourMilk)
+            % 5. Finish the drink (finishDrink)
 
-        %% HELPER FUCNTIONS
-        fucntion brewEspressoShot 
-            % programm movements for creating coffee  
-            pick cup
-            grind coffee
-            create Espress
         end
+
+        function icedCoffeeCreate()
+            % Function that creates an iced coffee drink.
+            % Includes steps:
+            % 1. Brew espresso (espressoBrew)
+            % 2. Add milk (addMilk)
+            % 3. Add ice cubes to the cup (addIce)
+            % 4. Pour milk into the cup (pourMilk)
+            % 5. Finish the drink (finishDrink)
+
+        end
+
+        function teaCreate()
+            % Function that creates a tea drink.
+            % Includes steps:
+            % 1. Place tea bag into the cup (putTeaBag)
+            % 2. Steam the liquid (steamLiquid)
+            % 3. Finish the drink (finishDrink)
+            
+        end
+
+        %% Helper functions assisting in the creation of drinks
+        
+        function espressoBrew()
+            % Function to brew an espresso.
+            % Involves the DobotNova2 performing these actions:
+            % 1. Grabbing a cup
+            % 2. Grinding coffee beans
+            % 3. Brewing espresso
+
+        end
+
+        function putTeaBag()
+            % Function to prepare tea.
+            % Involves the UR3e performing these actions:
+            % 1. Grab a tea bag
+            % 2. Place it into a cup
+
+        end
+
+        function addMilk()
+            % Function to pour milk into milk jug.
+            % Involves the UR3e performing these actions:
+            % 1. Place milk jug into milk dispenser 
+            % 2. Pour milk into milk jug 
+
+        end
+
+        function addIce()
+            % Function to add ice cubes to a cup.
+            % Involves the UR3e performing these actions:
+            % 1. Pick up cup
+            % 2. Place cup under ice cube dispenser
+
+        end
+
+        function steamLiquid()
+            % Function to steam the liquid (milk, water, etc.).
+            % Involves the UR3e performing these actions:
+            % 1. Pick up milk jug
+            % 2. Place jug under a steamer and wait
+
+        end
+
+        function pourMilk()
+            % Function to pour steamed milk into the cup.
+            % Involves the UR3e and DobotNova2 performing these actions:
+            % 1. Pours milk after steaming into a cup
+
+        end
+
+        function finishDrink()
+            % Function to finalize the drink preparation.
+            % Involves the UR3e performing these actions:
+            % 1. Puts a lid on the cup
+            % 2. Hands the finished drink to the customer
+
+        end
+
     end   
 end
--- Code/EnvSetup.m
+++ Code/EnvSetup.m
@@ -1,31 +1,32 @@
-function [nova2, ur3e] = EnvSetup
-    % Function to set up the environment with various objects (shelves, barriers,
-    % a person model, emergency stops, and a table).
+function [nova2, ur3e, cup, cupLid, milkJug, iceCube, portafilter] = EnvSetup
     hold on;  % Hold the current plot for multiple objects
     axis equal;  % Maintain equal scaling for all axes
     axis([-1.8 1.8 -1.8 1.8 -0.1 1.5]);  % Set the axis limits
     
+  
+    %% Movable objects
+    cup = PlaceObject('Cup.ply', [-0.1, 0.3, 0.65]); % add coffee cup model
+    cupLid = PlaceObject('CupLid.ply', [0.1, 0.3, 0.65]); % add coffee lid model
     
-    %% Place the shelf object on the table
-    % Load and place a shelf at the specified position
-    h = PlaceObject('Cup.ply', [-0.1, 0.3, 0.65]); 
-    h = PlaceObject('CupLid.ply', [0.1, 0.3, 0.65]); 
+    % cupWithLid = PlaceObject('CupWithLid.ply', [-0.1, 0.15, 0.65]); % add cup with lid model
+    milkJug = PlaceObject('MilkJug.ply', [0.1, 0.15, 0.65]); % add milk jug model
     
-    h = PlaceObject('CupWithLid.ply', [-0.1, 0.15, 0.65]); 
-    h = PlaceObject('MilkJug.ply', [0.1, 0.15, 0.65]); 
+    iceCube = PlaceObject('IceCube.ply', [0, 0.5, 0]); % add ice cube model
+    portafilter = PlaceObject('EspressoHandle.ply', [0, 0.4, 0.7]); % add portafilter model (coffee handle)
+
+
+    %% Non-movable objects
+    espressoMachine = PlaceObject('EspressoMachine.ply', [0.35, -0.5, 0.65]); % add espresso machine model
+    grinder = PlaceObject('Grinder.ply', [0.9, -0.2, 0.65]); % add coffee grinder model
+    iceCubeDispenser = PlaceObject('IceCubeDispenser.ply', [-0.75, -0.5, 0.65]); % add ice cube dispenser model
+    table = PlaceObject('Table.ply', [0, 0, 0.4]); % add table
     
-    %h = PlaceObject('IceCube.ply', [0, 0.5, 0]); 
-    h = PlaceObject('EspressoHandle.ply', [0, 0.4, 0.7]); 
-    h = PlaceObject('EspressoMachine.ply', [0.35, -0.5, 0.65]); 
-    
-    h = PlaceObject('Grinder.ply', [0.9, -0.2, 0.65]); 
-    h = PlaceObject('IceCubeDispenser.ply', [-0.75, -0.5, 0.65]); 
-    
-    h = PlaceObject('Table.ply', [0, 0, 0.4]); 
-    
+
     %% Adding DobotNova2 and UR3e robots to the environment 
     disp('Adding DobotNova2 robot to the environment');
-    nova2 = DobotNova2(transl(0.65, 0.3, 0.6));  % Create LinearUR3 robot with initial transform
-    ur3e = UR3e(transl(-0.65, 0.3, 0.6));
+    nova2 = DobotNova2(transl(0.65, 0.3, 0.6));  % Create DobotNova2 robot with initial transform
+    ur3e = UR3e(transl(-0.65, 0.3, 0.6)); % Create LinearUR3 robot with initial transform
+
+
 end
 
