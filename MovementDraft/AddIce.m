b747f5e3912afec62bc2d96d1e7549fc24cc50c1
AldorinoG <aldorinojonathangladwyn.luhur@student.uts.edu.au>
2024-10-22 23:21:50 +1100

    Update AddIce.m

--- MovementDraft/AddIce.m
+++ MovementDraft/AddIce.m
@@ -12,13 +12,27 @@ hold on;  % Hold the current plot for multiple objects
 axis equal;  % Maintain equal scaling for all axes
 axis([-1.8 1.8 -1.8 1.8 -0.1 1.5]);  % Set the axis limits
 view(210, 20)
+
 %% Teach function for nova2
 disp('Please teach the UR3e robot by moving it to the desired position and pressing Enter.');
 nova2.model.teach();  % Call teach function to manually set the position
 
-%% Define a trajectory for UR3e
+%% Define a trajectory for UR3e based on the provided joint angles (image)
 qWaypoints2 = [
-    deg2rad([0, 0, 0, 0, 0, 0]);              % Initial position
+    deg2rad([0, 0, 0, 0, 0, 0]);               % Initial position
+    deg2rad([0, -50.4, 122, 0, 0, 0]);         % Fold
+    deg2rad([162, -50.4, 122, 0, 0, 0]);      % Rotate
+    deg2rad([162, -57.7, 100, -43.2, 79.2, 0]);  % Grab cup
+    deg2rad([162, -93.7, 136, -43.2, 79.2, 0]); % Lift cup
+    deg2rad([61.2, -93.7, 136, -43.2, 79.2, 0]); % Rotate
+    deg2rad([61.2, -86.5, 108, -21.6, 79.2, 0]); % Wait for ice
+    %add time
+    deg2rad([61.2, -93.7, 136, -43.2, 79.2, 0]); % Pull
+    deg2rad([162, -93.7, 136, -43.2, 79.2, 0]); % Lift cup
+    deg2rad([162, -57.7, 100, -43.2, 79.2, 0]);  % Put cup down
+    deg2rad([162, -50.4, 122, 0, 0, 0]);      % Fold arm
+    deg2rad([0, -50.4, 122, 0, 0, 0]);         % Rotate
+    deg2rad([0, 0, 0, 0, 0, 0]);               % Return to initial position
 ];
 
 % Interpolate waypoints for UR3e
@@ -28,7 +42,13 @@ qMatrix2 = InterpolateWaypointRadians(qWaypoints2, deg2rad(4));  % For UR3e
 maxSteps = 600;  % You can adjust the number of steps for smoother/slower motion
 qMatrix2_resampled = resampleTrajectory(qMatrix2, maxSteps);
 
+% Animate the UR3e
+for i = 1:maxSteps
+    ur3e.model.animate(qMatrix2_resampled(i, :));  % Animate UR3e
 
+    % Add a pause for real-time visualization
+    pause(0.05);  % Adjust pause for slower/faster movement
+end
 
 %% Resampling function to match trajectory length
 function qMatrix_resampled = resampleTrajectory(qMatrix, numSteps)
